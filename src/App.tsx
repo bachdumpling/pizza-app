0dabfe25b6c9a59bd7dabc6863c2b0d4aba24ace
Bach Le <bachle@mac.mynetworksettings.com>
2024-11-15 21:53:42 -0500

    project set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Button } from "./components/ui/button";
+import "./index.css";
+
 function App() {
-  return (<p>Hello World</p>)
+  return (
+    <div>
+      <p>Hello World</p>
+      <div>
+        <Button variant="outline">Click me</Button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
